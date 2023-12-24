ba7f6ee3f6769126a701d5b348cfb9b6adeba850
Ishwaryasankar <118426168+ishwaryasankar@users.noreply.github.com>
2023-12-24 14:37:42 +0530

    Add files via upload

---
+++ Problem Solving/problemday1.java/prblm3.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class prblm3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+String wrd1 = scanner.nextLine();
+ System.out.println(wrd1); 
+         String wrd2 = scanner.nextLine();
+ System.out.println(wrd2); 
+         String wrd3 = scanner.nextLine();
+ System.out.println(wrd3); 
+}
+}
